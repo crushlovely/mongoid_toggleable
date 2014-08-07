a7713e69d98453c0c3d6f1da03f85ba1405b7cad
PJ Kelly <pj@crushlovely.com>
2014-08-06 17:36:05 -0700

    Implemented a nicer API with dynamically created scope names and added documentation.

--- lib/mongoid/toggleable.rb
+++ lib/mongoid/toggleable.rb
@@ -7,25 +7,49 @@ module Mongoid
     end
 
     module ClassMethods
-      def toggleable(*toggleable_attributes)
-        defaults = { :default => true }
-        options = toggleable_attributes.extract_options!.dup
+      # Public: Create a toggleable attribute on the model.
+      #
+      # toggleable_attribute - A symbol representing the name of the attribute to be created.
+      # options - The Hash options used to customize the attribute (default: {}):
+      #           :default             - The Boolean default of the attribute.
+      #                                  (optional, default: true).
+      #           :scope_name          - The Symbol representing the name of the positive scope
+      #                                  (optional, default: #{toggleable_attribute}.
+      #           :inverse_scope_name  - The Symbol representing the name of the positive scope
+      #                                  (optional, default: #{toggleable_attribute}.
+      #
+      # Examples:
+      #
+      #   toggleable :for_sale, :default => false
+      #   toggleable :visible, :inverse_scope_name => :hidden
+      #   toggleable :publishable, :default => false, :inverse_scope_name => :unpublishable
+      #   toggleable :featured, :default => true, :scope_name => :features
+      #
+      # Defines the attribute specified as well as relevant scopes.
+      def toggleable(toggleable_attribute, options = {})
+        defaults = {
+          :default => true,
+          :scope_name => toggleable_attribute,
+          :inverse_scope_name => "not_#{toggleable_attribute}"
+        }
         options = defaults.merge(options)
 
-        toggleable_attributes.each do |toggleable_attribute|
-          field toggleable_attribute, :type => MongoidToggleable.boolean_type, :default => options[:default]
-        end
-      end
-
-      def find_toggleable(toggleable_attribute, state)
-        where(toggleable_attribute => state)
+        field toggleable_attribute, :type => MongoidToggleable.boolean_type, :default => options[:default]
+        scope options[:scope_name], -> { where(toggleable_attribute => true) }
+        scope options[:inverse_scope_name], -> { where(toggleable_attribute => false) }
       end
     end
 
+    # Public: Toggle the attribute specified
+    #
+    # Returns the value the attribute was changed to. Does not persist the change.
     def toggle(toggleable_attribute)
       send("#{toggleable_attribute}=", !send(toggleable_attribute))
     end
 
+    # Public: Toggle the attribute specified and persist the change
+    #
+    # Returns the result of #save.
     def toggle!(toggleable_attribute)
       toggle(toggleable_attribute)
       save
--- spec/lib/mongoid_toggleable_spec.rb
+++ spec/lib/mongoid_toggleable_spec.rb
@@ -5,7 +5,7 @@ describe Mongoid::Toggleable do
     include Mongoid::Document
     include Mongoid::Toggleable
 
-    toggleable :visible
+    toggleable :visible, :inverse_scope_name => :hidden
   end
 
   class Product
@@ -19,8 +19,8 @@ describe Mongoid::Toggleable do
     include Mongoid::Document
     include Mongoid::Toggleable
 
-    toggleable :publishable, :default => false
-    toggleable :featured, :default => true
+    toggleable :publishable, :default => false, :inverse_scope_name => :unpublishable
+    toggleable :featured, :default => true, :scope_name => :features
   end
 
   describe 'field setup' do
@@ -36,13 +36,41 @@ describe Mongoid::Toggleable do
     end
   end
 
-  describe '.find_toggleable' do
+  describe 'dynamic scopes' do
     let!(:visible_article) { Article.create }
     let!(:hidden_article) { Article.create(:visible => false) }
+    let!(:for_sale_product) { Product.create(:for_sale => true) }
+    let!(:not_for_sale_product) { Product.create }
 
-    it 'returns records based on the parameters passed in' do
-      expect(Article.find_toggleable(:visible, true)).to include(visible_article)
-      expect(Article.find_toggleable(:visible, true)).not_to include(hidden_article)
+    describe 'scope definition' do
+      it 'creates scopes based on the parameters passed in' do
+        expect(Article).to respond_to(:visible)
+        expect(Article).to respond_to(:hidden)
+        expect(Product).to respond_to(:for_sale)
+        expect(Product).to respond_to(:not_for_sale)
+        expect(Story).to respond_to(:publishable)
+        expect(Story).to respond_to(:unpublishable)
+        expect(Story).to respond_to(:features)
+        expect(Story).to respond_to(:not_featured)
+      end
+    end
+
+    describe 'positive scope' do
+      it 'returns the appropriate records' do
+        expect(Article.visible).to include(visible_article)
+        expect(Article.visible).not_to include(hidden_article)
+        expect(Product.for_sale).to include(for_sale_product)
+        expect(Product.for_sale).not_to include(not_for_sale_product)
+      end
+    end
+
+    describe 'negative scope' do
+      it 'returns the appropriate records' do
+        expect(Article.hidden).to include(hidden_article)
+        expect(Article.hidden).not_to include(visible_article)
+        expect(Product.not_for_sale).to include(not_for_sale_product)
+        expect(Product.not_for_sale).not_to include(for_sale_product)
+      end
     end
   end
 
